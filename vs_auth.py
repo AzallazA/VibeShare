46d2ee01da436bfcc0b366177f977e29075ec370
Zachary Johnson <zjohns20@uncc.edu>
2021-04-26 14:11:00 -0400

    VS Login App

--- vs_auth.py
+++ vs_auth.py
@@ -9,7 +9,7 @@ import tekore as tk
 
 client_id = 'b74cf8069d564daaa6bcc7eb21e80c52'
 client_secret = '217c6d35964545128c1efc70908ebfbc'
-redirect = 'https:%2F%2Fvibeshareapp.com%2Fthankyou.html'
+redirect = 'https:%2F%2Fvibeshareapp.com%2Flogin.html'
 response_type = 'token'
 scope = 'playlist-modify-private%20playlist-modify-public%20playlist-read-collaborative%20playlist-read-private%20ugc-image-upload%20user-follow-modify%20user-follow-read%20user-library-modify%20user-library-read%20user-modify-playback-state%20user-read-currently-playing%20user-read-email%20user-read-playback-position%20user-read-playback-state%20user-read-private%20user-read-recently-played%20user-top-read'
 state = 'ZA_2buN-j5U4W37boUkV7a9OW1oyojBt8kY9rSNZBps'
@@ -22,9 +22,10 @@ redirect_uri = ('https://accounts.spotify.com/authorize?' +
 
 app = QApplication(sys.argv)
 web = QWebEngineView()
-web.resize(500, 700)
+web.resize(800, 700)
 
 web.load(QUrl(redirect_uri))
 web.show()
+
 
 sys.exit(app.exec_())
